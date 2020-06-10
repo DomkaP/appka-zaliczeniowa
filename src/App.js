47b42f66860aee477945ea59202b5376184de6b4
DomkaP <dominika.pietrzyk11@gmail.com>
2020-06-10 16:55:00 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import './App.css';
+import Zamowienie from './components/zamowienie';
+import Skomponuj from './components/skomponuj';
+
+function App() {
+
+  const [ pizza, setPizza ] = useState(null);
+
+  const otrzymajPizze = pizza => {
+    setPizza(pizza);
+  }
+
+  return (
+    <div className="App">
+      
+      <div className="content">
+        <Skomponuj otrzymajPizze={otrzymajPizze}/>
+        <Zamowienie nowaPizza={pizza}/>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
